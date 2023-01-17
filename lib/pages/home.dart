d5fbafe6bd7fd8d99ce14f463c7d8ec2cf680041
abdikaafi452 <cabdikaafimohamed452@gmail.com>
2023-01-17 11:13:34 +0300

    login_flutter_Assigment

---
+++ lib/pages/home.dart
@@ -0,0 +1,34 @@
+//Name :Abdkiaafi Mohamed Hassan
+//ID: C119560
+//Class: CA192
+
+import 'dart:js';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:mylogin_191/pages/login.dart';
+
+class home extends StatelessWidget {
+  const home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => LoginPage()));
+              },
+              icon: Icon(Icons.logout_outlined))
+        ],
+        title: Text('somali'),
+      ),
+      body: const Center(
+        child: Text("loged"),
+      ),
+    );
+  }
+}
